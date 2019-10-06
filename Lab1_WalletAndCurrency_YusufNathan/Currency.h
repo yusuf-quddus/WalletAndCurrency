211ad1d5ab29973bd0b205f43fe9741b06747536
Yusuf Quddus <yusuf_quddus@yahoo.com>
2019-10-06 16:22:23 -0700

    Update Currency.h

--- Lab1_WalletAndCurrency_YusufNathan/Currency.h
+++ Lab1_WalletAndCurrency_YusufNathan/Currency.h
@@ -1,13 +1,82 @@
-void Currency::operator= (const Currency* c1) 
-{
-wholePart = c1->wholePart;
-fractionalPart = c1->fractionalPart;
-currencyCoin = c1->currencyCoin;
-currencyNote = c1->currencyNote;
+#pragma once
 
-}
+#include <iostream>
+#include <string>
 
+class Currency {
+protected:
+	std::string currencyNote;
+	std::string currencyCoin;
+	int wholePart;
+	int fractionalPart;
+public:
+	Currency();
+	Currency(int, int);
 
+	// getters
+	std::string getCurrencyNote();
+	std::string getCurrencyCoin();
+	int getWholePart();
+	int getFractionalPart();
 
+	// setters
+	void setCurrencyNote(std::string);
+	void setCurrencyCoin(std::string);
+	void setWholePart(int);
+	void setFractionalPart(int);
 
-void operator= (const Currency* c1);
+	//  made class friends cause easier
+	/*friend class Dollar;
+	friend class Euro;
+	friend class Yen;
+	friend class Rupee;
+	friend class Yuan;
+	*/
+
+	// operator overloads
+	Currency& operator= (const Currency * &c1);
+	friend std::istream& operator>>(std::istream &in, Currency &c);
+	friend std::ostream& operator<<(std::ostream& out, const Currency& c);
+	friend Currency operator+ (Currency&, Currency&);
+	friend Currency operator+ (Currency& c1, double d);
+	friend Currency operator+ (double d, Currency& c1);
+	friend Currency operator- (Currency&, Currency&);
+	friend Currency operator-(double d, Currency& c1);
+	friend Currency operator-(Currency& c1, double d);
+	friend bool operator== (Currency&, Currency&);
+	friend bool operator> (Currency&, Currency&);
+	friend bool operator< (Currency&, Currency&);
+	friend bool operator>= (Currency&, Currency&);
+	friend bool operator<= (Currency&, Currency&);
+};
+
+// derived class. how to pass variables in constructor to base?
+class Dollar : public Currency {
+public:	
+	Dollar();
+	Dollar(int, int);
+};
+
+class Euro : public Currency {
+public:
+	Euro();
+	Euro(int, int);
+};
+
+class Yen : public Currency {
+public:
+	Yen();
+	Yen(int, int);
+};
+
+class Rupee : public Currency {
+public:
+	Rupee();
+	Rupee(int, int);
+};
+
+class Yuan : public Currency {
+public:
+	Yuan();
+	Yuan(int, int);
+};
